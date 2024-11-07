7d51bc0128d921499c242c26c269b7d2dafaa307
Hidenori Watanave <hwtnv2006@gmail.com>
2024-11-07 18:22:03 +0900

    AR App

---
+++ AR/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2107052.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 202828678, 202828657, 202828709, 202828687, 202828656, 202828690, 202828705, 202828668, 202828707, 202828674, 202828677, 202828711, 202828701, 202828683 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
