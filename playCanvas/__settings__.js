2cfdc00318b60c65539bc1dc6bf60a1396722a0f
Hidenori Watanave <hwtnv2006@gmail.com>
2024-11-07 12:52:20 +0900

    playCanvasからエクスポートしたコンテンツ

---
+++ playCanvas/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2107038.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 202826632, 202826633, 202826634, 202826635, 202826707 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
